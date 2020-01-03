8f509fa5d6d401688c0c90a3468c0005ebc6ffb1
adminl <adminl@localhost>
2020-01-03 16:02:20 +0100

    Monop

---
+++ _monopoly/src/fr/gtm/monopoly/Joueur.java
@@ -0,0 +1,37 @@
+package fr.gtm.monopoly;
+
+public class Joueur {
+	private String nom;
+	private Pion pion;
+	
+	public Joueur(String nom) {
+		this.nom = nom;
+	}
+	
+	public Joueur(String nom, Pion pion) {
+		this.nom = nom;
+		this.pion = pion;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void jouer(Gobelet gobelet) {
+
+	}
+
+	public Pion getPion() {
+		return pion;
+	}
+
+	public void setPion(Pion pion) {
+		this.pion = pion;
+	}
+	
+	
+}
---
+++ _monopoly/src/fr/gtm/monopoly/Pion.java
@@ -0,0 +1,39 @@
+package fr.gtm.monopoly;
+
+public class Pion {
+	private String nom;
+	private Joueur joueur;
+	private Case position;
+	
+	public Pion(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Joueur getJoueur() {
+		return joueur;
+	}
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+
+
+	public Case getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(Case position) {
+		this.position = position;
+	}
+
+	public void avancer(int r) {
+
+	}
+
+}
